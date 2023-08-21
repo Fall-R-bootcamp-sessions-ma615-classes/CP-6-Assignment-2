c60524366ac8cffcbcf03c6e2ce65dc01bb111fe
aidanohara1 <aohara1@bu.edu>
2023-08-21 13:07:23 -0400

    little talks

--- cp6-assignment-2.R
+++ cp6-assignment-2.R
@@ -11,15 +11,15 @@ buoy <- read.csv("freshBuoyData.csv")
 # Exercise 2
 
 # Remove using subset
-#betterBuoy <- subset()
+# betterBuoy <- subset()
 
 # Exercise 3
 
 # Your code for fixing the 99s:
 
-#maxWindSpeed <-
+# maxWindSpeed <-
 
 # Exercise 4
-#install.packages("GrapheR")
-#library(GrapheR)
+# install.packages("GrapheR")
+# library(GrapheR)
 
